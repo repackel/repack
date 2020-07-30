954ab0adf785fe256ed63cf2d34510bfd93f2a0e
huc <ht@Live.se>
2020-07-30 18:56:38 +0800

    add date type

--- Table/index.vue
+++ Table/index.vue
@@ -1,68 +1,69 @@
 <template>
-    <div class="cp-full">
-        <leftTree class="cp-tree" :class="{hidden}" v-if="cfg.tree" :treeName="cfg.treeName" :treeLev="cfg.treeLev" v-on="$listeners"></leftTree>
-        <div class="toggle-icon" :class="{hidden}" @click="hidden = !hidden" v-if="cfg.tree">
-            <i :class="!hidden?'el-icon-d-arrow-left':'el-icon-d-arrow-right'"></i>
-        </div>
-        <div class="cp-page-inner" :class="{hasLeft:cfg.tree}">
-            <div class="cp-action-bar" :class="cfg.actionAlign" v-if="_get(cfg, 'actionList.length')">
-                <div v-for="(x,i) in (cfg.actionAlign ==='between' ? cfg.actionList:[cfg.actionList]  )" :key="i">
-                    <el-button :type="y.type||'primary'" :plain="y.plain" :icon="y.icon" size="small" @click="y.fn ? y.fn() : $emit(y.e)" v-for="(y,j) in x" :key="j">{{y.name}}</el-button>
-                </div>
-            </div>
-            <cpTitle v-if="1 || _get(cfg, 'actionList.length')">{{cfg.rightName}}</cpTitle>
-            <el-form :inline="true" class="cp-form" label-width="6em">
-                <template v-for="(x,i) in cfg.searchList">
-                    <el-form-item :label="x.name" :key="i">
-                        <template v-if="x.type==='input'">
-                            <el-input v-model="queryParams[x.key]" :placeholder="'请输入'+x.name" :maxlength="x.key==='mobile'?11:20" v-bind="inputcfg(x, i)" @keyup.enter.native="getList" />
-                        </template>
-                        <template v-else-if="x.type==='select'">
-                            <el-select v-model="queryParams[x.key]" :placeholder="'请选择'+x.name" v-bind="inputcfg(x, i)">
-                                <el-option v-for="dict in ( x.list instanceof Array ? x.list : queryList[x.list] || [] )" :key="dict.val" :label="dict.name" :value="dict.val" />
-                            </el-select>
-                        </template>
-                        <template v-else-if="x.type==='date'">
-                            <el-date-picker v-model="queryParams[x.key]" type="daterange" start-placeholder="开始日期" end-placeholder="结束日期" v-bind="inputcfg(x, i)">
-                            </el-date-picker>
-                        </template>
-                        <template v-else-if="x.type==='division'">
-                            <cp-divisions v-model="queryParams[x.key]" :placeholder="'请选择'+x.name" :lev="3" :checkStrictly="true" v-bind="inputcfg(x, i)" />
-                        </template>
-                    </el-form-item>
-                    <br class="break" :key="i" v-if="x.br" />
-                </template>
-                <el-form-item class="btns">
-                    <el-button type="primary" icon="el-icon-search" size="mini" @click="getList" v-if="searchCfg.queryText || searchCfg.queryBtn + '' === '2' ">{{searchCfg.queryText || '筛选'}}</el-button>
-                    <el-button type="plain" icon="el-icon-refresh-right" size="mini" @click="resetQuery" v-if="searchCfg.resetText || searchCfg.queryBtn + '' === '2'">{{searchCfg.resetText || '重置'}}</el-button>
-                </el-form-item>
-            </el-form>
-            <!-- <cpTitle v-if="!_get(cfg, 'actionList.length')">{{cfg.rightName}}</cpTitle> -->
-            <el-table :data="tableData" style="width: 100%" stripe v-loading="loading" @selection-change="handleSelectionChange">
-                <el-table-column type="selection" width="55" v-if="cfg.tableSelection" fixed="left" />
-                <el-table-column type="index" width="80" label="序号" />
-                <template v-for="(x,i) in cfg.tableList">
-                    <el-table-column v-bind="colcfg(x,i)" :key="i" v-if="x.buttonList" :fixed="x.fixed==='false'? false : x.fixed || 'right'">
-                        <template slot-scope="scope">
-                            <el-button size="mini" :type="y.type|| 'text'" v-for="(y,j) in x.buttonList" :key="j" @click="y.fn ? y.fn(scope.row,scope.$index) : void 0" :icon="y.icon" v-if="!y.hidden || !(y.hidden && y.hidden(scope.row) )">
-                                {{ typeof y.text === 'function' ? y.text(scope.row,scope.$index) : y.text }}
-                            </el-button>
-                        </template>
-                    </el-table-column>
-                    <el-table-column v-bind="colcfg(x,i)" :key="i" v-else-if="x.transform">
-                        <template slot-scope="scope">
-                            <span :style="typeof x.style ==='function' ? x.style(scope.row):x.style" :class="x.class" @click="x.fn ? x.fn(scope.row,scope.$index) : void 0">{{ x.transform(scope.row) }}</span>
-                        </template>
-                    </el-table-column>
-                    <el-table-column v-bind="colcfg(x,i)" :key="i" v-else />
-                </template>
-            </el-table>
-            <div class="pager-container">
-                <el-pagination @size-change="handleSizeChange" @current-change="handleCurrentChange" :current-page="queryParams.pageNum" :page-sizes="[10, 20, 50, 100]" :page-size="queryParams.pageSize" layout="total, sizes, prev, pager, next, jumper" :total="tableTotal">
-                </el-pagination>
-            </div>
-        </div>
+  <div class="cp-full">
+    <leftTree class="cp-tree" :class="{hidden}" v-if="cfg.tree" :treeName="cfg.treeName" :treeLev="cfg.treeLev" v-on="$listeners"></leftTree>
+    <div class="toggle-icon" :class="{hidden}" @click="hidden = !hidden" v-if="cfg.tree">
+      <i :class="!hidden?'el-icon-d-arrow-left':'el-icon-d-arrow-right'"></i>
     </div>
+    <div class="cp-page-inner" :class="{hasLeft:cfg.tree}">
+      <div class="cp-action-bar" :class="cfg.actionAlign" v-if="_get(cfg, 'actionList.length')">
+        <div v-for="(x,i) in (cfg.actionAlign ==='between' ? cfg.actionList:[cfg.actionList]  )" :key="i">
+          <el-button :type="y.type||'primary'" :plain="y.plain" :icon="y.icon" size="small" @click="y.fn ? y.fn() : $emit(y.e)" v-for="(y,j) in x" :key="j">{{y.name}}</el-button>
+        </div>
+      </div>
+      <cpTitle v-if="1 || _get(cfg, 'actionList.length')">{{cfg.rightName}}</cpTitle>
+      <el-form :inline="true" class="cp-form" label-width="6em">
+        <template v-for="(x,i) in cfg.searchList">
+          <el-form-item :label="x.name" :key="i">
+            <template v-if="x.type==='input'">
+              <el-input v-model="queryParams[x.key]" :placeholder="'请输入'+x.name" :maxlength="x.key==='mobile'?11:20" v-bind="inputcfg(x, i)" @keyup.enter.native="getList" />
+            </template>
+            <template v-else-if="x.type==='select'">
+              <el-select v-model="queryParams[x.key]" :placeholder="'请选择'+x.name" v-bind="inputcfg(x, i)">
+                <el-option v-for="dict in ( x.list instanceof Array ? x.list : queryList[x.list] || [] )" :key="dict.val" :label="dict.name" :value="dict.val" />
+              </el-select>
+            </template>
+            <template v-else-if="x.type==='date'">
+              <el-date-picker type="daterange" v-model="searchDateArr" start-placeholder="开始日期" end-placeholder="结束日期" value-format="yyyy-MM-dd HH:mm:ss" v-bind="inputcfg(x, i)" @change="arr=>([queryParams[x.key1],queryParams[x.key2] ]=arr)">
+              </el-date-picker>
+            </template>
+            
+            <template v-else-if="x.type==='division'">
+              <cp-divisions v-model="queryParams[x.key]" :placeholder="'请选择'+x.name" :lev="3" :checkStrictly="true" v-bind="inputcfg(x, i)" />
+            </template>
+          </el-form-item>
+          <br class="break" :key="i" v-if="x.br" />
+        </template>
+        <el-form-item class="btns">
+          <el-button type="primary" icon="el-icon-search" size="mini" @click="getList" v-if="searchCfg.queryText || searchCfg.queryBtn + '' === '2' ">{{searchCfg.queryText || '筛选'}}</el-button>
+          <el-button type="plain" icon="el-icon-refresh-right" size="mini" @click="resetQuery" v-if="searchCfg.resetText || searchCfg.queryBtn + '' === '2'">{{searchCfg.resetText || '重置'}}</el-button>
+        </el-form-item>
+      </el-form>
+      <!-- <cpTitle v-if="!_get(cfg, 'actionList.length')">{{cfg.rightName}}</cpTitle> -->
+      <el-table :data="tableData" style="width: 100%" stripe v-loading="loading" @selection-change="handleSelectionChange">
+        <el-table-column type="selection" width="55" v-if="cfg.tableSelection" fixed="left" />
+        <el-table-column type="index" width="80" label="序号" />
+        <template v-for="(x,i) in cfg.tableList">
+          <el-table-column v-bind="colcfg(x,i)" :key="i" v-if="x.buttonList" :fixed="x.fixed==='false'? false : x.fixed || 'right'">
+            <template slot-scope="scope">
+              <el-button size="mini" :type="y.type|| 'text'" v-for="(y,j) in x.buttonList" :key="j" @click="y.fn ? y.fn(scope.row,scope.$index) : void 0" :icon="y.icon" v-if="!y.hidden || !(y.hidden && y.hidden(scope.row) )">
+                {{ typeof y.text === 'function' ? y.text(scope.row,scope.$index) : y.text }}
+              </el-button>
+            </template>
+          </el-table-column>
+          <el-table-column v-bind="colcfg(x,i)" :key="i" v-else-if="x.transform">
+            <template slot-scope="scope">
+              <span :style="typeof x.style ==='function' ? x.style(scope.row):x.style" :class="x.class" @click="x.fn ? x.fn(scope.row,scope.$index) : void 0">{{ x.transform(scope.row) }}</span>
+            </template>
+          </el-table-column>
+          <el-table-column v-bind="colcfg(x,i)" :key="i" v-else />
+        </template>
+      </el-table>
+      <div class="pager-container">
+        <el-pagination @size-change="handleSizeChange" @current-change="handleCurrentChange" :current-page="queryParams.pageNum" :page-sizes="[10, 20, 50, 100]" :page-size="queryParams.pageSize" layout="total, sizes, prev, pager, next, jumper" :total="tableTotal">
+        </el-pagination>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -73,6 +74,7 @@ export default {
     return {
       loading: false,
       hidden: false,
+      searchDateArr:[],
       queryParams: {
         pageNum: 1,
         pageSize: 10
@@ -88,14 +90,14 @@ export default {
       tableTotal: 0,
       dialogVisible: false,
       searchCfg: {
-        queryBtn:2
+        queryBtn: 2
       },
       currentPage: 1
     };
   },
   computed: {},
   mounted() {
-    this.searchCfg = this.cfg.searchCfg || this.searchCfg ;
+    this.searchCfg = this.cfg.searchCfg || this.searchCfg;
     // 搜索下拉框的列表
     const searchList =
       (this.cfg.searchList &&
@@ -117,6 +119,7 @@ export default {
       // 搜索函数
       if (this.cfg.searchFn) {
         this.loading = true;
+
         this.cfg
           .searchFn(this.queryParams, reset)
           .then(res => {
@@ -163,6 +166,7 @@ export default {
     resetQuery() {
       this.queryParams = this._pick(this.queryParams, ["pageSize"]);
       this.queryParams.pageNum = 1;
+      this.searchDateArr =[]
       this.getList("reset");
     },
     handleSelectionChange(val) {
