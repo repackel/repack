ed3f7e2fc5d5612f0fa0ae602096c27a014c7223
huc <ht@Live.se>
2020-09-02 17:33:56 +0800

    fix date endtiem

--- Table/index.vue
+++ Table/index.vue
@@ -176,6 +176,12 @@ export default {
     },
     dateChange(arr, x) {
       [this.queryParams[x.key1], this.queryParams[x.key2]] = arr || [];
+      if (this.queryParams[x.key2]) {
+        this.queryParams[x.key2] = this.queryParams[x.key2].replace(
+          "00:00:00",
+          "23:59:59"
+        );
+      }
     },
     handleSizeChange(size) {
       this.queryParams.pageNum = 1;
