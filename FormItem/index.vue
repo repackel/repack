2590c3be5e49091a810c5619ac362593de3e168e
huc <ht@Live.se>
2020-09-29 15:28:38 +0800

    fix error

--- FormItem/index.vue
+++ FormItem/index.vue
@@ -7,13 +7,13 @@
       <span v-else>{{x.transform ? x.transform(form[x.key]) :form[x.key] || locz('na')}}</span>
     </template>
     <template v-if="x.type==='input'">
-      <el-input v-model="form[x.key]" v-bind="inputcfg(x)" />
+      <el-input v-model="form[x.key]" v-bind="inputcfg(x)" @change="val=> x.changeFn ? x.changeFn(val) : void 0"  @input="val=> x.inputFn ? x.inputFn(val) : void 0"/>
     </template>
     <template v-if="x.type==='number'">
       <el-input-number v-model="form[x.key]" :min="(x.range&&x.range[0])||1" :max="(x.range&&x.range[1])||10" v-bind="inputcfg(x)" />
     </template>
     <template v-if="x.type==='textarea'">
-      <el-input v-model="form[x.key]" type="textarea" resize='none' rows="3" v-bind="inputcfg(x)"  @change="val=> x.changeFn ? x.changeFn(val) : void 0"  @input="val=> x.inputFn ? x.inputFn(val) : void 0"/>
+      <el-input v-model="form[x.key]" type="textarea" resize='none' rows="3" v-bind="inputcfg(x)" />
     </template>
     <template v-if="x.type==='radio'">
       <el-radio-group v-model="form[x.key]" v-bind="inputcfg(x)">
